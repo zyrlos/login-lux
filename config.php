50e0a41b82a2048a1122bce7461eacd0630866d3
zyrlos <112430921+zyrlos@users.noreply.github.com>
2024-04-21 16:42:42 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'login';
+
+ $conexao = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+
+ /*if($conexao -> connect_error){
+    echo "Erro";
+ }else{
+    echo "Conectado com sucesso";
+ }*/
+?>
---
+++ login.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(to right,rgb(0, 80, 172), rgb(10, 10, 10));
+        }
+        .box{
+            color: white;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 15px;
+            border-radius: 15px;
+            width: 20%;
+        }
+        fieldset{
+            border: 3px solid white;
+        }
+        legend{
+            border: 1px solid white;
+            padding:10px;
+            text-align: center;
+            background-color: dodgerblue;
+            border-radius: 8px;
+        }
+        .inputBox{
+            position: relative;
+        }
+        .inputUser{
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: white !important;
+            letter-spacing: 2px;
+        }
+        .labelInput{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+        }
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput{
+            top: -20px;
+            font-size: 12px;
+            color: dodgerblue;
+        }
+        #data_nascimento{
+            border: none;
+            padding: 8px;
+            border-radius: 10px;
+            outline: none;
+            font-size: 15px;
+        }
+        .submit{
+            background-image: linear-gradient(to right,rgb(0, 92, 197), rgb(90, 20, 220));
+            width: 100%;
+            border: none;
+            padding: 15px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+            border-radius: 10px;
+        }
+        .submit:hover{
+            background-image: linear-gradient(to right,rgb(0, 80, 172), rgb(10, 10, 10));
+        }
+        a{
+            color: white;
+        }
+    </style>
+</head>
+<body>
+<a href="home.php">Voltar</a>
+    <div class="box">
+    <fieldset>
+        <legend><h1>Login</h1></legend>
+        <br>
+        <form action="testLogin.php" method="POST">
+        <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email" class="labelInput">Email</label>
+        </div>
+            <br><br>
+        <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha" class="labelInput">Senha</label>
+        </div>
+        <br><br>
+            <input class="submit" type="submit" name="submit" value="Enviar">
+        </form>
+    </fieldset>
+    </div>
+</body>
+</html>
